67ea5a79986be859ece91dff9ce93bfc7a570202
holygodfxck <95016892+holygodfxck@users.noreply.github.com>
2022-10-01 01:20:56 +0800

    Update YtbAdsban.js

--- YtbAdsban.js
+++ YtbAdsban.js
@@ -1,4 +1,4 @@
-
+hostname = *googlevideo.com
 
 #youtube去广告
 (^https?:\/\/(?!((r\d-)|manifest))[\w-]+\.googlevideo\.com\/(?!dclk_video_ads).+?)&ctier=L(&.+?),ctier(,.+?)((&probe=1)|$) url 302 $1$4$5
